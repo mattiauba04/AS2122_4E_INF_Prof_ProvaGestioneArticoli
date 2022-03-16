2e03efb562890788166df2022d6d58d08aa1574c
mattiauba <mattiaubalducci6@gmail.com>
2022-03-16 09:28:56 +0100

    // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -9,18 +9,21 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     class Articolo
     {
         // TODO: (8) aggiungere attributi privati di Articolo
-        // ...
         int codice;
         string descrizione;
         string unitaMisura;
         double prezzo;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
-        // ...
+        public string Descrizione { get; }
+        public string UnitaMisura { get; }
+        double Prezzo { get; }
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            Descrizione = descrizione;
+            Uni
 
         }
 
