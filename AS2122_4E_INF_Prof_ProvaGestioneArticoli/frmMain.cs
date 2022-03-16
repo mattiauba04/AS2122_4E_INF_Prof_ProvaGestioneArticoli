94569597fc6eab0e63128aaba0e39b153e4f1499
mattiauba <mattiaubalducci6@gmail.com>
2022-03-16 09:44:10 +0100

    (2) aggiungere visualizzazione articoli inseriti nella listbox

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -50,6 +50,11 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 case "Visualizza articoli":
                     // TODO: (2) aggiungere visualizzazione articoli inseriti nella listbox
                     // ... lstElenco.Items.Add(a.Visualizzati()); 
+                    lstElenco.Items.Clear();
+                    foreach (Articolo a in articoli)
+                    {
+                        lstElenco.Items.Add(a.Visualizzati());
+                    }
                     break;
             }
         }
