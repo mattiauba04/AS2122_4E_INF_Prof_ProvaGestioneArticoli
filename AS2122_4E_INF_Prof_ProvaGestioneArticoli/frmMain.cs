e45f1515b2907101809e2c1f2c7155a936ec9ae2
mattiauba <mattiaubalducci6@gmail.com>
2022-03-16 09:40:40 +0100

    aggiungi l'articolo all'elenco di articoli  (1) aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -34,7 +34,7 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 // TODO: (1) aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli
                 // ...
 
-                Articolo a = new Articolo();
+                Articolo a = new Articolo(articoli.Count, lblDescrizione.Text,lblUnitaMisura.Text,Convert.ToDouble(lblPrezzo.Text));
 
                 articoli.Add(a);
                 
